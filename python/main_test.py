ca38c7ea1a0b4cbd07cec147c652fe7ba39ba54c
mimo-owl <m-shirasaka@g.ecc.u-tokyo.ac.jp>
2025-03-14 11:09:12 +0900

    finished step6

--- python/main_test.py
+++ python/main_test.py
@@ -6,39 +6,39 @@ import os
 import pathlib
 
 # STEP 6-4: uncomment this test setup
-# test_db = pathlib.Path(__file__).parent.resolve() / "db" / "test_mercari.sqlite3"
+test_db = pathlib.Path(__file__).parent.resolve() / "db" / "test_mercari.sqlite3"
 
-# def override_get_db():
-#     conn = sqlite3.connect(test_db)
-#     conn.row_factory = sqlite3.Row
-#     try:
-#         yield conn
-#     finally:
-#         conn.close()
+def override_get_db():
+    conn = sqlite3.connect(test_db)
+    conn.row_factory = sqlite3.Row
+    try:
+        yield conn
+    finally:
+        conn.close()
 
-# app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_db] = override_get_db
 
-# @pytest.fixture(autouse=True)
-# def db_connection():
-#     # Before the test is done, create a test database
-#     conn = sqlite3.connect(test_db)
-#     cursor = conn.cursor()
-#     cursor.execute(
-#         """CREATE TABLE IF NOT EXISTS items (
-# 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-# 		name VARCHAR(255),
-# 		category VARCHAR(255)
-# 	)"""
-#     )
-#     conn.commit()
-#     conn.row_factory = sqlite3.Row  # Return rows as dictionaries
+@pytest.fixture(autouse=True)
+def db_connection():
+    # Before the test is done, create a test database
+    conn = sqlite3.connect(test_db)
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS items (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR(255),
+		category VARCHAR(255)
+	)"""
+    )
+    conn.commit()
+    conn.row_factory = sqlite3.Row  # Return rows as dictionaries
 
-#     yield conn
+    yield conn
 
-#     conn.close()
-#     # After the test is done, remove the test database
-#     if test_db.exists():
-#         test_db.unlink() # Remove the file
+    conn.close()
+    # After the test is done, remove the test database
+    if test_db.exists():
+        test_db.unlink() # Remove the file
 
 client = TestClient(app)
 
@@ -52,32 +52,34 @@ client = TestClient(app)
 def test_hello(want_status_code, want_body):
     response_body = client.get("/").json()
     # STEP 6-2: confirm the status code
+    assert response_body.status_code == want_status_code, f"Expected status code {want_status_code}, got {response_body.status_code}"
     # STEP 6-2: confirm response body
+    assert response_body.json() == want_body, f"Expected response body {want_body}, got {response_body.json()}"
 
 
 # STEP 6-4: uncomment this test
-# @pytest.mark.parametrize(
-#     "args, want_status_code",
-#     [
-#         ({"name":"used iPhone 16e", "category":"phone"}, 200),
-#         ({"name":"", "category":"phone"}, 400),
-#     ],
-# )
-# def test_add_item_e2e(args,want_status_code,db_connection):
-#     response = client.post("/items/", data=args)
-#     assert response.status_code == want_status_code
-    
-#     if want_status_code >= 400:
-#         return
-    
-    
-#     # Check if the response body is correct
-#     response_data = response.json()
-#     assert "message" in response_data
+@pytest.mark.parametrize(
+    "args, want_status_code",
+    [
+        ({"name":"used iPhone 16e", "category":"phone"}, 200),
+        ({"name":"", "category":"phone"}, 400),
+    ],
+)
+def test_add_item_e2e(args,want_status_code,db_connection):
+    response = client.post("/items/", data=args)
+    assert response.status_code == want_status_code
 
-#     # Check if the data was saved to the database correctly
-#     cursor = db_connection.cursor()
-#     cursor.execute("SELECT * FROM items WHERE name = ?", (args["name"],))
-#     db_item = cursor.fetchone()
-#     assert db_item is not None
-#     assert dict(db_item)["name"] == args["name"]
+    if want_status_code >= 400:
+        return
+
+
+    # Check if the response body is correct
+    response_data = response.json()
+    assert "message" in response_data
+
+    # Check if the data was saved to the database correctly
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM items WHERE name = ?", (args["name"],))
+    db_item = cursor.fetchone()
+    assert db_item is not None
+    assert dict(db_item)["name"] == args["name"]
